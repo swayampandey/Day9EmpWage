a40c08a2d899805c8bde863128f045e2477f85e2
swayampandey <swayam265@gmail.com>
2022-07-06 17:13:02 +0530

    Added : UC7

---
+++ src/employeewage/EmpWage2.java
@@ -0,0 +1,14 @@
+package employeewage;
+
+
+
+public class EmpWage2 {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to employee wage computation");
+		EmpWage a = new EmpWage();
+		a.EmpW();
+
+	}
+
+}
