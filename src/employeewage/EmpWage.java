4c932dab21796a7f8a6f8ced5b7471c8b34394c2
swayampandey <swayam265@gmail.com>
2022-07-06 16:48:54 +0530

    Added : UC4

--- src/employeewage/EmpWage.java
+++ src/employeewage/EmpWage.java
@@ -9,24 +9,27 @@ public class EmpWage {
 				 * if o then Employee is Absent
 				 * if 1 then present
 				 * if 2 then Employee is doing Half day
+				 * initializing variables
 				 */
 			int employeePresent=1;
 			int employeePartTime=2;
 			int WagePerHour=20;
 			int EmpHour=8;
-			int ParttimeHour=4;
-			int ispresent= (int) (Math.floor(Math.random()*10)%3);
-			if (ispresent==employeePresent) {
+			int PartTimeHour=4;
+			int ispresent=(int) (Math.floor(Math.random()*10)%3);
+			switch (ispresent) {
+			case 0:
 				System.out.println("Employee is Present");
-				System.out.println("Employee wage for day=" +WagePerHour*EmpHour);
-			} else if (ispresent==employeePartTime) {
-				System.out.println("Employee is doing Part time");
-				System.out.println("Employee Partime wage=" +WagePerHour*ParttimeHour);
-				
-			}
-			else {
+				System.out.println("Employee wage for Day=" +WagePerHour*EmpHour);
+				break;
+			case 1:
+				System.out.println("Employee is doing part time");
+				System.out.println("Employee Part time wage=" +PartTimeHour*WagePerHour);
+				break;
+			default:
 				System.out.println("Employee is Absent");
-				System.out.println("Employee Wage for day = 0");
+				System.out.println("Employee wage for Day=0");
+			
 			}
 		}
 	}
