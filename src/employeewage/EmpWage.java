ef18113de71dcebbfc5098b3fc1cb9cae91ae2e5
swayampandey <swayam265@gmail.com>
2022-07-06 16:38:36 +0530

    Added UC2

--- src/employeewage/EmpWage.java
+++ src/employeewage/EmpWage.java
@@ -10,13 +10,18 @@ public class EmpWage {
 				 * if 1 then present
 				 * if 2 then Employee is doing Half day
 				 */
-				int employeePresent=1;
-				int ispresent = (int) (Math.floor(Math.random()*10)%2);
-				
-			if(ispresent == employeePresent)
+			int employeePresent=1;
+			int wagePerhour=20;
+			int empHour=8;
+			int ispresent= (int) (Math.floor(Math.random()*10)%2);
+			if(ispresent==employeePresent) {
 				System.out.println("Employee is Present");
-			else
+			System.out.println("Employee Wage for day=" +empHour*wagePerhour);
+			}
+			else {
 				System.out.println("Employee is Absent");
+				System.out.println("Employee Wage for day= 0");
 
 		}
 	}
+}
